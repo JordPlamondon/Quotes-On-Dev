6ac78a7cb8d0790177ec4f21b341f5d5d00ecbd6
Jordan <jordplamondon@gmail.com>
2017-11-24 14:52:35 -0800

    style submit page

--- page-submit.php
+++ page-submit.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <?php if( is_user_logged_in() && current_user_can('edit_posts' ) ): ?>
     <div class="quotes-submission-wrapper">
     <form name="quoteForm" id="quote-submission-form" >
-      <div>
+      <div class="author-container">
         <label for="quote-author">Author of Quote</label>
         <input type="text" name="quote_author" id="quote-author" >
       </div>
@@ -20,11 +20,11 @@ get_header(); ?>
         <textarea rows="3" cols="20" name="quote_content" id="quote-content" ></textarea>
       </div>
       <div>
-        <label for="quote-source">where did u find this quote</label>
+        <label for="quote-source">Where did u find this quote?(e.g. book name)</label>
         <input type="url" name="quote_source" id="quote-source" ></input>
       </div>
       <div>
-        <label for="quote-source-url">where did u find this quote</label>
+        <label for="quote-source-url">Provide the source of the URL of the quote, if available</label>
         <input type="url" name="quote_source-url" id="quote-source-url" ></teaxtarea>
       </div>
       <input type="submit" value="submit quote" id="submit-quote">      
