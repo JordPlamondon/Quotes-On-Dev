010d774a7223dec374007b93222679d2d096b087
Jordan <jordplamondon@gmail.com>
2017-11-25 21:30:53 -0800

    render title & content on click js. add submit quote function

--- js/api.js
+++ js/api.js
@@ -1,23 +1,60 @@
-// (function($){
+(function ($) {
 
-// // slide for posting -> 34/38 look at nytimes to see how to get the data. Filter it to get random post -> 29/38
+  $('#new-quote-button').on('click', function (e) {
+    e.preventDefault();
 
-//   $.ajax()
+    $.ajax({
+      method: 'GET',
+      url: api_vars.root_url + 'wp/v2/posts/?filter[orderby]=rand&filter[posts_per_page]=1',
+    })
+      .done(function (data) {
 
-//   $('#close-comments').on('click', function(event) {
-//     event.preventDefault();
-//     $.ajax({
-//        method: 'post',
-//        url: red_vars.rest_url + 'wp/v2/posts/' + red_vars.post_id,
-//        data: {
-//           comment_status: 'closed'
-//        },
-//        beforeSend: function(xhr) {
-//           xhr.setRequestHeader( 'X-WP-Nonce', red_vars.wpapi_nonce );
-//        }
-//     }).done( function(response) {
-//        alert('Success! Comments are closed for this post.');
-//     });
-//  });
+        var slug = data[0].slug;
+        var renderedPost = data.shift();
 
-// })(jQuery);
+        $('.entry-title').html(renderedPost.title.rendered);
+        $('.entry-content').html(renderedPost.content.rendered);
+        $('.hentry').append(history.pushState(null, null, slug));
+
+        if (renderedPost._qod_quote_source_url) {
+          $('.source').html('<a href="' + renderedPost._qod_quote_source_url + '">' + renderedPost._qod_quote_source + '</a>');
+        }
+
+      })
+  });
+
+  //submit a quote
+  $('#submit-quote').on('click', function (e) {
+
+    e.preventDefault();
+
+    var quoteAuthor = $('#quote-author').val();
+    var quoteContent = $('#quote-content').val();
+    var quoteSource = $('#quote-source').val();
+    var quoteSourceUrl = $('#quote-source-url').val();
+
+    $.ajax({
+      method: 'POST',
+      url: api_vars.root_url + 'wp/v2/posts',
+      data: {
+        title: quoteAuthor,
+        content: quoteContent,
+        _qod_quote_source: quoteSource,
+        _qod_quote_source_url: quoteSourceUrl,
+        status: 'draft'
+      },
+
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);
+      }
+
+    }).done(function () {
+
+
+    }).always(function () {
+      $('#quote-submission-form').trigger('reset');
+    });
+
+  });
+
+})(jQuery);
